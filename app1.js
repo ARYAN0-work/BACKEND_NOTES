fba6711619755078de46334888f0bf2cf8cf395b
ARYAN0-WORK <carrer1270@gmail.com>
2026-03-20 13:58:04 +0530

    add global variable notes

---
+++ app1.js
@@ -0,0 +1,20 @@
+// GLOBALS - NO WINDOW !!!!
+
+// __dirname - path to current directory
+// __filename - file name
+// require - function to use modules (CommonJS) /// cover most of the
+// module - info about current module (file) /// cover most of the
+// process - info about env where the program is being executed // very imppppp => it gives about the info about the program where is excuted
+
+//=> based on your enviourment you can make bunch of diffrent application
+
+console.log(__dirname);
+
+// in vanialla js we have 
+// set interval nd set timeout
+
+setInterval(()=> {
+    console.log('hello world');
+},1000)
+
+// now hello world prints every 1 sec
---
+++ notes1.md
@@ -0,0 +1,9 @@
+when we work with vanialjs we have a cool thing access thing to window object it is cool bcz in window objects we can have bunch of things
+
+=> for ex i can get a queryselector we can get a node then we can get a fetch  
+
+=> in node theres no window bcz theres no browser do theres no window onject and if you try ti acceses it it will give error and hence it will give crash your app
+
+=>yeah but there are globals => global variables[in your application you can access it any time anywhere doesnt matter how complex it get]
+
+see app1.js
