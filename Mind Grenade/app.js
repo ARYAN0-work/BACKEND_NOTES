b37c842cd09f51fb43bc4b4342dcf73cf9f83a2e
ARYAN0-WORK <carrer1270@gmail.com>
2026-03-23 16:24:11 +0530

    adding notes of mind grenade

---
+++ Mind Grenade/app.js
@@ -0,0 +1,11 @@
+require('./7-mind-grenade')
+
+// when we are setting everything if we have a fn inside of a module that we invoke  even though we didnt assign it to a variable you can see it clewarly we didnt invoke that variable
+
+//imp 3 thigs
+
+/*
+1. this is mot unique also can do in es6 modules
+2. also used in third party modules
+3. when you import a module you just invoke it only
+*/
