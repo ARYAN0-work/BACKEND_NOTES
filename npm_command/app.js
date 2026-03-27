b84b31df0948619500660e1edefe263a8226925a
ARYAN0-WORK <carrer1270@gmail.com>
2026-03-27 14:03:25 +0530

    adding notes of npm command

---
+++ npm_command/app.js
@@ -0,0 +1,12 @@
+// npm- global command, comes with node [ when we install node we also have access to npm which give acces to npms golbal command]
+// npm --version
+
+// local dependency - use it only in this particular project [we can install pks as an local dependcy which mean that we'll only use that pkg in this particular project that we're working on and on that command for that one would be npm i and then whatever the pkg name]
+// npm i <packageName>
+
+// gloabal dependency - use it any project [or we can install dependcy as an global dependcy]
+// npm install -g <packageName> [ g is the flag for global]
+// sudo npm install -g <packageName> (mac)
+
+// you can do things  with globally with npx but its not preffered (LATER)
+
