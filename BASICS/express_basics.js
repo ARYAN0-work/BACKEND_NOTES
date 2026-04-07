25a9ba6a6fda0eebb91e50841ecae9882901d135
Aryan Mishra <carrer1270@gmail.com>
2026-04-08 02:42:27 +0530

    adding imporvised notes of express basics

--- BASICS/express_basics.js
+++ BASICS/express_basics.js
@@ -1,20 +1,25 @@
 const express = require('express')
 const app =express()
 
-app.get('/',()=>{
+app.get('/',()=>{// get = read data = what user is trying to do
     console.log('user hit the resource');
     
     res.send('Home page')
 })// i nedd twoi things a path so what resouce user is trying to access and it would make sense if we would start with root correct and then and 2nd thing is callback fn= this callback fn will get invoked every time as user is performing get requesyt on our route so basically on our dfomain and the this callbacjk fn gets same with 2 arguments
 
+
+// use of network tab
+
+//## about page 
 app.get('/about',(req,res)=>{
     res.status(200).send('About Page')
 })
 
-app.all('*',(req,res)=>{
-    res.status(404).send('<h1>resource not found</h1>')
+// 404 page
+app.all('*',(req,res)=>{// altering default 404 and setting up our own 404= bcz user can do multiple things on a server
+    res.status(404).send('<h1>resource not found</h1>')// first is path and 2nd one is callback fn=> we also added stause code make sense it will not return 200
 })
-app.listen(5000,()=>{// poet we are listenkng on
+app.listen(5000,()=>{// port we are listenkng on
     console.log('server is listening on port 5000....');
     
 })
